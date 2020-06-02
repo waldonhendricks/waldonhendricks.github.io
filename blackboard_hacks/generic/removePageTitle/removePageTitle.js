433b0c9787b15e9ef1ab2232b1bd40463e3b3aef
Waldon <hendricksw@cput.ac.za>
2020-06-02 15:37:50 +0200

    BB hacks
    updated code for BB hacks

---
+++ blackboard_hacks/generic/removePageTitle/removePageTitle.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">/*<![CDATA[*/
+    /* Insert CSS to remove grey page title bar */
+    var link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "https://waldonhendricks.github.io/blackboard_hacks/generic/removePageTitle/removePageTitle.css");
+    var head = document.getElementsByTagName("head") [0];
+    head.appendChild(link);
+/*]]>*/</script>
